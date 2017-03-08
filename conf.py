73bd28f52735d2ab8470246afb7a101852822446
aturner-epcc <a.turner@epcc.ed.ac.uk>
2017-03-08 10:07:30 +0000

    Changed icon

--- conf.py
+++ conf.py
@@ -131,7 +131,7 @@ todo_include_todos = True
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'images/whiteEPCCtransp.png'
+html_logo = 'images/cirrus_logo_white-Transparent-Background.png
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
